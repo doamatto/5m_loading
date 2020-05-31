54eec8173d4e49487b5bf8b7f9fd7f65311cf803
doamatto <hello@doamatto.xyz>
2020-05-31 00:47:18 -0700

    lots of more stuff

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games { 'common' }
+
+author { 'doamatto' }
+description { 'Another FiveM loading screen' }
+version '0.1.0'
+
+loadscreen 'loadscreen.html'
+
+files {
+    'loadscreen.html',
+    'css/style.css',
+    'img/logo.png',
+    'script.js',
+}
